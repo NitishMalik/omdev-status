8fe5277e1f672cb4937b8ace9a0569c00b69e4ba
Anjali Chawla <anjali2589@gmail.com>
2018-10-06 20:20:11 +0530

    adding profile carousel to profile page'

--- client/src/store.js
+++ client/src/store.js
@@ -10,8 +10,8 @@ const store = createStore(
 	initialState,
 	compose(
 		applyMiddleware(...middleware),
-		window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : null
-	) // compose is used to use Redux dev tool
+		window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : f=>f
+	)
 );
 
 export default store;
